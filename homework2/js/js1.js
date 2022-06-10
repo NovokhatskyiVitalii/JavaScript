52788dc7a609d3c19dba23df59eb6dab17ed6687
NovokhatskyiVitalii <105878524+NovokhatskyiVitalii@users.noreply.github.com>
2022-06-10 19:55:32 +0300

    Update js1.js

--- homework2/js/js1.js
+++ homework2/js/js1.js
@@ -19,8 +19,6 @@ const fruitsupper = fruits.map(element => {
 });
 console.log(fruitsupper);
 
-let num = [1, 2, 3, 4];
-plusNumber(num);
 function plusNumber(array) {
     let plus = array.map(function (num) {
         return num + 1;
@@ -28,14 +26,25 @@ function plusNumber(array) {
     console.log(plus);
 }
 
-function add(x, y, z, w) {
-    return x + y + z + w;
+let num = [1, 2, 3, 4];
+plusNumber(num);
+
+function add() {
+    let sum = 0;
+    for (argument of arguments) {
+      if (typeof argument == 'number') {
+        sum += argument;
+      }
+    }
+    return sum;
 }
 console.log(add(1, 2, 3, 4));
 
 const onlynum = [1, "hello", 2, 3, 4, "5", "6", 7, null];
 console.log(onlynum);
-console.log((onlynum.filter(item => typeof item === "number")));
+console.log((onlynum.filter(function (item)  {
+    return  typeof item === "number"
+  })));
 
 let havetruevalue = arrayTest([0, false, null, 1, true]);
 function arrayTest(myArray){
