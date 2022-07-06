f997e6f4f340825f908e6ccc0b5df03645e70504
NovokhatskyiVitalii <105878524+NovokhatskyiVitalii@users.noreply.github.com>
2022-07-06 08:48:27 +0300

    Update js.js

--- homework7/js/js.js
+++ homework7/js/js.js
@@ -1,24 +1,24 @@
 //task1
 let myNodelist = document.getElementsByTagName("LI");
 for (i = 0; i < myNodelist.length; i++) {
-    let = span = document.createElement("span");
-    let = txt = document.createTextNode("\u00D7");
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
 
-let = close = document.getElementsByClassName("close");
+let close = document.getElementsByClassName("close");
 let = i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-        let = div = this.parentElement;
+        let div = this.parentElement;
         div.style.display = "none";
     }
 }
 
 
-let = list = document.querySelector("ul");
+let list = document.querySelector("ul");
 list.addEventListener('click', function (ev) {
     if (ev.target.tagName === "LI") {
         ev.target.classList.toggle('checked');
@@ -27,9 +27,9 @@ list.addEventListener('click', function (ev) {
 
 
 function newElement() {
-    let = li = document.createElement("li");
-    let = inputValue = document.getElementById("myInput").value;
-    let = t = document.createTextNode(inputValue);
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
     li.appendChild(t);
     setTimeout(() => li.style.opacity = 1);
     if (inputValue === "") {
@@ -39,15 +39,15 @@ function newElement() {
     }
     document.getElementById("myInput").value = "";
 
-    let = span = document.createElement("span");
-    let = txt = document.createTextNode("\u00D7");
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function () {
-            let = div = this.parentElement;
+            let div = this.parentElement;
             div.style.display = "none";
         }
     }
